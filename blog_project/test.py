1c9768f5d22c299062375e1d21bedab889e9190b
tony_honor <93711779@qq.com>
2020-07-05 19:23:04 +0800

    7.5.19第一次上传7.5.19第一次上传7.5.19第一次上传7.5.19第一次上传7.5.19第一次上传

---
+++ blog_project/test.py
@@ -0,0 +1,13 @@
+class Point:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+    def print_point(self):
+        print((self.x,self.y))
+
+p=Point(3,9)
+p.name='A'
+a=[1,2,]
+b=[1,2]
+print(a)
+print(b)
